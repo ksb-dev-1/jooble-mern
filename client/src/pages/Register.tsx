0bf2be622b67b74c459ee30d6e94bad30a2f45db
ksb-dev-1 <ksbabaleshwar@gmail.com>
2024-03-20 13:49:14 +0530

    Landing, Register, Login page complete

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,11 +1,54 @@
-import { Link } from "react-router-dom";
+import { Form, Link } from "react-router-dom";
+
+// 3rd party libraries
+//import { toast } from 'react-toastify';
+
+// utils
+//import customFetch from '../utils/customFetch';
+
+// assets
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+
+// components
+import FormRow from "../components/FormRow";
+import Logo from "../components/Logo";
+//import SubmitBtn from '../components/SubmitBtn';
+
+// export const action = async ({ request }: any) => {
+//   const formData = await request.formData();
+//   const data = Object.fromEntries(formData);
+
+//   try {
+//     await customFetch.post('/auth/register', data);
+//     toast.success('Registration successful');
+//     return redirect('/login');
+//   } catch (error) {
+//     toast.error(error?.response?.data?.msg);
+
+//     return error;
+//   }
+// };
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <Form method="post" className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="last name" />
+        <FormRow type="text" name="location" />
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
+        {/* <SubmitBtn /> */}
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
   );
 };
 export default Register;
